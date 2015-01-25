5029121684aa30833e6ed3251f2f187fe95c5b1e
akonanga <akonanga@sbcglobal.net>
2015-01-25 14:43:49 -0800

    cached_randomPizzaContainer

--- fnmportfolioCRP/views/js/main.js
+++ fnmportfolioCRP/views/js/main.js
@@ -472,10 +472,11 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+      var cached_randomPizzaContainer = document.querySelectorAll(".randomPizzaContainer");
+    for (var i = 0; i < cached_randomPizzaContainer.length; i++) {
+      var dx = determineDx(cached_randomPizzaContainer[i], size);
+      var newwidth = (cached_randomPizzaContainer[i].offsetWidth + dx) + 'px';
+      cached_randomPizzaContainer[i].style.width = newwidth;
     }
   }
 
