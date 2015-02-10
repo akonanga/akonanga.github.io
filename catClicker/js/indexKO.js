9fcf0f8119bc887a545f0c8baf29f6c9b9a44ea9
akonanga <akonanga@sbcglobal.net>
2015-02-09 21:09:18 -0800

    fix level

--- catClicker/js/indexKO.js
+++ catClicker/js/indexKO.js
@@ -5,24 +5,34 @@ var ViewModel = function () {
     this.clickCount = ko.observable(0);
     this.name = ko.observable('Tabby');
     this.imgSrc = ko.observable('images/cat1.jpg');
-    this.level = ko.observable('Infant');
     this.nicknames = ko.observableArray([
             {nickname: 'Cookie Monster'},
             {nickname: 'Big Bird'},
             {nickname: 'Oscar The Grouch'}
         ]);
+    this.level = ko.computed(function () {
+        var level;
+        var clickCnt = this.clickCount();
+        if(clickCnt < 2) {
+            level = 'Infant';
+        } else
+        if(clickCnt < 4) {
+            level = 'Toddler';
+        } else
+        if(clickCnt < 13) {
+            level = 'Child';
+        } else
+        if(clickCnt < 20) {
+            level = 'Teen';
+        } else
+        if(clickCnt >= 20) {
+            level = 'Adult';
+        }
+        return level;
+    }, this);
 
     this.incrementCounter = function () {
         this.clickCount(this.clickCount() + 1);
-        if(this.clickCount() > 2 && this.clickCount() <= 12) {
-            this.level('Toddler');
-        } else
-        if(this.clickCount() > 12 && this.clickCount() <= 20) {
-            this.level('Teen');
-        } else
-        if(this.clickCount() > 20) {
-            this.level('Adult');
-        }
     };
 };
 
